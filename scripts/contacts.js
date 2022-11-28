deca800d334f6deb402ced21f6e5b3cec21a223b
AkhilaChiluveru <akhila.chiluveru@pdx.edu>
2022-11-27 23:30:07 -0800

    Email error message added to meet 30 lines js code

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -3,15 +3,28 @@ form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   let username = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
   let namediv = document.getElementById("namediv");
+  let emaildiv = document.getElementById("emaildiv");
+  const nameregex = "/[a-zA-Z]";
+  const emailregex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-  if (username.length > 0 && !username.match("[a-zA-Z]")) {
+  if (username.length > 0 && !username.match(nameregex)) {
     let errormessage = document.createElement("p");
-    console.log(errormessage + "," + username + "namediv" + namediv);
     errormessage.innerHTML = "Name allows only alphabet";
     errormessage.style.color = "red";
     errormessage.style.fontSize = "20px";
     namediv.append(errormessage);
+    event.preventDefault();
+  }
+
+  if (email.length > 0 && !email.match(emailregex)) {
+    let errormessage = document.createElement("p");
+    errormessage.innerHTML = "Please enter valid email";
+    errormessage.style.color = "red";
+    errormessage.style.fontSize = "20px";
+    emaildiv.append(errormessage);
     event.preventDefault();
   }
 }
